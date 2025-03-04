5733f865208ced7a34e2fe0e48231f99efc3ced9
HuyVu002 <huyhuyhuyak74@gmail.com>
2025-03-04 14:44:03 +0700

    Update

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,3 +1,8 @@
+import React, { useState, useRef, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FaAddressBook, FaCog } from "react-icons/fa";
+
 export const Button = () => {
     return <button>Button </button>
 }
+
